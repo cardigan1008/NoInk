0c74d16497275f724ed868c5e1a50ac40bd9858d
HW <huangwei021230@HWdeMacBook-Pro.local>
2023-11-14 10:58:39 +0800

    feat: home page init

--- app/src/main/java/com/bagel/noink/ui/home/HomeFragment.kt
+++ app/src/main/java/com/bagel/noink/ui/home/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -12,26 +13,38 @@ import com.bagel.noink.databinding.FragmentHomeBinding
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private lateinit var homeViewModel: HomeViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+        homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
+        val imageView: ImageView = binding.clickableImage
+        val bar: View = binding.bottomBar
+
+        // Set text from ViewModel
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        // Set OnClickListener for the image
+        imageView.setOnClickListener {
+            // Handle click action here
+        }
+
+        // Customize your bar as needed
+        // bar.visibility = View.VISIBLE
+        // ...
+
         return root
     }
 

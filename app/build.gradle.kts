dff5ad718bff64fc77c99f6c2a087e6dcb0c719e
Nboxff <1814739992@qq.com>
2023-11-16 16:31:36 +0800

    feat: add network support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,10 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
     implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
     implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
+    // okHttp所需依赖
+    implementation("com.squareup.okhttp3:okhttp:4.9.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }

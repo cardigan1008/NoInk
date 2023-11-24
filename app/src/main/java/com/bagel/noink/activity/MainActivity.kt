f5a5fd8ba7edb2a10a52430a5d1fa63cdefd616e
cardigan1008 <211250058@smail.nju.edu.cn>
2023-11-24 23:37:21 +0800

    feat: change the name of bar

--- app/src/main/java/com/bagel/noink/activity/MainActivity.kt
+++ app/src/main/java/com/bagel/noink/activity/MainActivity.kt
@@ -38,7 +38,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_home, R.id.nav_history, R.id.nav_stars, R.id.nav_personal_account
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)

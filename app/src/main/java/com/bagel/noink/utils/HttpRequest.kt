8442301c47893908455b517d09a00a8427363045
cardigan1008 <211250058@smail.nju.edu.cn>
2023-11-24 21:36:20 +0800

    feat: add http request util

---
+++ app/src/main/java/com/bagel/noink/utils/HttpRequest.kt
@@ -0,0 +1,73 @@
+package com.bagel.noink.utils
+
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.Response
+import org.json.JSONObject
+import java.io.IOException
+import java.util.concurrent.TimeUnit
+
+class HttpRequest {
+    interface CallbackListener {
+        fun onSuccess(responseJson: JSONObject)
+        fun onFailure(errorMessage: String)
+    }
+
+    private val client = OkHttpClient.Builder().readTimeout(5, TimeUnit.SECONDS).build()
+
+    fun get(url: String, callbackListener: CallbackListener) {
+        val request = Request.Builder().url(url).build()
+        val call = client.newCall(request)
+
+        call.enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                val errorMessage = "Failed to connect to the backend"
+                callbackListener.onFailure(errorMessage)
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                handleResponse(response, callbackListener)
+            }
+        })
+    }
+
+    fun post(url: String, requestBody: RequestBody, callbackListener: CallbackListener) {
+        val request = Request.Builder().url(url).post(requestBody).build()
+        val call = client.newCall(request)
+
+        call.enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                val errorMessage = "Failed to connect to the backend"
+                callbackListener.onFailure(errorMessage)
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                handleResponse(response, callbackListener)
+            }
+        })
+    }
+
+    private fun handleResponse(response: Response, callbackListener: CallbackListener) {
+        if (response.isSuccessful) {
+            val responseBody = response.body?.string()
+            if (responseBody != null) {
+                try {
+                    val responseJson = JSONObject(responseBody)
+                    callbackListener.onSuccess(responseJson)
+                } catch (e: Exception) {
+                    val errorMessage = "Failed to parse JSON response"
+                    callbackListener.onFailure(errorMessage)
+                }
+            } else {
+                val errorMessage = "Empty response body"
+                callbackListener.onFailure(errorMessage)
+            }
+        } else {
+            val errorMessage = "Request failed: ${response.code}"
+            callbackListener.onFailure(errorMessage)
+        }
+    }
+}

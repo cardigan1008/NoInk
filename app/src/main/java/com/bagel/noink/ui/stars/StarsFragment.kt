b7d19bfc255fd79f479b4b15755dd1dc32dff504
cardigan1008 <211250058@smail.nju.edu.cn>
2023-11-27 17:04:07 +0800

    fix: rename modules and delete old stars

--- app/src/main/java/com/bagel/noink/ui/stars/StarsFragment.kt
+++ app/src/main/java/com/bagel/noink/ui/stars/StarsFragment.kt
@@ -1,4 +1,4 @@
-package com.bagel.noink.ui.gallery
+package com.bagel.noink.ui.stars
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.bagel.noink.databinding.FragmentGalleryBinding
+import com.bagel.noink.databinding.FragmentStarsBinding
 
-class GalleryFragment : Fragment() {
+class StarsFragment : Fragment() {
 
-    private var _binding: FragmentGalleryBinding? = null
+    private var _binding: FragmentStarsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,9 +23,9 @@ class GalleryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+            ViewModelProvider(this).get(StarsViewModel::class.java)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentStarsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textGallery

2140d8c1e9e1d7a38c4be6348844bb7315887f3b
Nboxff <1814739992@qq.com>
2023-12-03 21:18:34 +0800

    feat: add EditInformationActivity, and jump to this activity

---
+++ app/src/main/java/com/bagel/noink/activity/EditInformationActivity.kt
@@ -0,0 +1,12 @@
+package com.bagel.noink.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bagel.noink.R
+
+class EditInformationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_information)
+    }
+}
--- app/src/main/java/com/bagel/noink/ui/account/AccountFragment.kt
+++ app/src/main/java/com/bagel/noink/ui/account/AccountFragment.kt
@@ -11,9 +11,11 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.bagel.noink.R
+import com.bagel.noink.activity.EditInformationActivity
 import com.bagel.noink.activity.LoginActivity
 import com.bagel.noink.activity.RegisterActivity
 import com.bagel.noink.databinding.FragmentAccountBinding
+import com.bagel.noink.ui.PersonalItemView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class AccountFragment : Fragment() {
@@ -24,6 +26,7 @@ class AccountFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    var itemUsername: PersonalItemView ?= null
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -46,10 +49,22 @@ class AccountFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        // 绑定相关组件
+        itemUsername = activity?.findViewById<PersonalItemView>(R.id.item_username)
+
+        // 跳转到修改用户名界面
+        itemUsername?.setOnClickListener {
+            startActivity(Intent(this.context, EditInformationActivity::class.java))
+        }
+
+
         // TODO: 删去测试按钮代码
+        // 登录按钮和注册按钮的跳转响应
         activity?.findViewById<Button>(R.id.testButton)
             ?.setOnClickListener {
-                startActivity(Intent(this.context, RegisterActivity::class.java))
+                val intent = Intent(this.context, RegisterActivity::class.java)
+                intent.putExtra("type", "username")
+                startActivity(intent)
             }
 
         activity?.findViewById<Button>(R.id.loginButton)

10b0a3124d91853acd96c6ccaea82c3d469223bf
Nboxff <1814739992@qq.com>
2023-12-04 11:38:22 +0800

    feat: add dialog window to update gender, add username and wechatId update

--- app/src/main/java/com/bagel/noink/ui/account/AccountFragment.kt
+++ app/src/main/java/com/bagel/noink/ui/account/AccountFragment.kt
@@ -1,13 +1,12 @@
 package com.bagel.noink.ui.account
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.bagel.noink.R
@@ -16,7 +15,6 @@ import com.bagel.noink.activity.LoginActivity
 import com.bagel.noink.activity.RegisterActivity
 import com.bagel.noink.databinding.FragmentAccountBinding
 import com.bagel.noink.ui.PersonalItemView
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class AccountFragment : Fragment() {
 
@@ -26,7 +24,13 @@ class AccountFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
-    var itemUsername: PersonalItemView ?= null
+    var itemUsername: PersonalItemView? = null
+    var itemWechatId: PersonalItemView? = null
+    var itemGender: PersonalItemView? = null
+    var itemBirthday: PersonalItemView? = null
+
+    var newGender: Boolean = true
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -50,11 +54,51 @@ class AccountFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
 
         // 绑定相关组件
-        itemUsername = activity?.findViewById<PersonalItemView>(R.id.item_username)
+        itemUsername = activity?.findViewById(R.id.item_username)
+        itemWechatId = activity?.findViewById(R.id.item_wechat)
+        itemGender = activity?.findViewById(R.id.item_gender)
+        itemBirthday = activity?.findViewById(R.id.item_birthday)
 
         // 跳转到修改用户名界面
         itemUsername?.setOnClickListener {
-            startActivity(Intent(this.context, EditInformationActivity::class.java))
+            val intent = Intent(this.context, EditInformationActivity::class.java)
+            intent.putExtra("type", "username")
+            startActivity(intent)
+        }
+
+        // 跳转到修改微信账号界面
+        itemWechatId?.setOnClickListener {
+            val intent = Intent(this.context, EditInformationActivity::class.java)
+            intent.putExtra("type", "wechat")
+            startActivity(intent)
+        }
+
+        // 弹出修改性别弹窗
+        itemGender?.setOnClickListener {
+            val genderOptions = arrayOf("男", "女")
+            val checkedItem = 0
+            var tempGender = true
+            val builder = AlertDialog.Builder(this.activity)
+            builder.setTitle("修改性别")
+            builder.setSingleChoiceItems(genderOptions, checkedItem) { dialog, which ->
+                // 处理选中的性别
+                val selectedGender = genderOptions[which]
+                tempGender = selectedGender == "男"
+            }
+
+            builder.setPositiveButton("确定") { dialog, which ->
+                // 点击 "确定" 按钮后关闭对话框
+                newGender = tempGender
+                dialog.dismiss()
+            }
+
+            builder.setNegativeButton("取消") { dialog, which ->
+                // 点击 "取消" 按钮后关闭对话框
+                dialog.dismiss()
+            }
+
+            val dialog = builder.create()
+            dialog.show()
         }
 
 
@@ -63,7 +107,6 @@ class AccountFragment : Fragment() {
         activity?.findViewById<Button>(R.id.testButton)
             ?.setOnClickListener {
                 val intent = Intent(this.context, RegisterActivity::class.java)
-                intent.putExtra("type", "username")
                 startActivity(intent)
             }
 

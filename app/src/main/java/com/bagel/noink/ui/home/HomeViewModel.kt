63def1a34df802a442b311ce80c76fadcb290086
quasdo <211250106@smail.nju.edu.cn>
2023-12-07 00:52:19 +0800

    feat：首页ui修改

--- app/src/main/java/com/bagel/noink/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/bagel/noink/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "记录下今天的故事吧~"
     }
     val text: LiveData<String> = _text
 }

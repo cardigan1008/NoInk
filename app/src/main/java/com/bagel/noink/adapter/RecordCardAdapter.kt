c541cbbbc9d04113c9da7ac7123c88bf20832b44
Xie Bofan <1814739992@qq.com>
2023-12-16 22:12:16 +0800

    feat: update home card UI

--- app/src/main/java/com/bagel/noink/adapter/RecordCardAdapter.kt
+++ app/src/main/java/com/bagel/noink/adapter/RecordCardAdapter.kt
@@ -4,18 +4,25 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.os.bundleOf
+import androidx.core.view.size
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.bagel.noink.R
 import com.bagel.noink.bean.ListItemBean
+import com.bagel.noink.ui.generate.TextGenViewModel
 import com.bumptech.glide.Glide
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class RecordCardAdapter(private var cards: List<ListItemBean>, private val navController: NavController, private val onItemClick: (position: Int) -> Unit) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class RecordCardAdapter(
+    private var cards: List<ListItemBean>,
+    private val navController: NavController,
+    private val onItemClick: (position: Int) -> Unit
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     companion object {
         private const val VIEW_TYPE_CARD_NEW = 1
         private const val VIEW_TYPE_CARD_RECORD = 2
@@ -32,13 +39,17 @@ class RecordCardAdapter(private var cards: List<ListItemBean>, private val navCo
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return when (viewType) {
             VIEW_TYPE_CARD_NEW -> {
-                val view = LayoutInflater.from(parent.context).inflate(R.layout.item_card_new, parent, false)
+                val view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.item_card_new, parent, false)
                 CardViewHolder1(view)
             }
+
             VIEW_TYPE_CARD_RECORD -> {
-                val view = LayoutInflater.from(parent.context).inflate(R.layout.item_card, parent, false)
+                val view =
+                    LayoutInflater.from(parent.context).inflate(R.layout.item_card, parent, false)
                 CardViewHolder2(view)
             }
+
             else -> throw IllegalArgumentException("Invalid view type")
         }
     }
@@ -50,6 +61,7 @@ class RecordCardAdapter(private var cards: List<ListItemBean>, private val navCo
                 // 绑定 CardViewHolder1 的数据和视图
                 holder.bind(card)
             }
+
             is CardViewHolder2 -> {
                 // 绑定 CardViewHolder2 的数据和视图
                 holder.bind(card, position)
@@ -90,13 +102,16 @@ class RecordCardAdapter(private var cards: List<ListItemBean>, private val navCo
         private val titleTextView: TextView = itemView.findViewById(R.id.title)
         private val imageView: ImageView = itemView.findViewById(R.id.imageView)
         private val contentView: TextView = itemView.findViewById(R.id.content)
+        private val tag_style: TextView = itemView.findViewById(R.id.tag_style)
+        private val tag_mood: TextView = itemView.findViewById(R.id.tag_mood)
 
         fun bind(card: ListItemBean, position: Int) {
             // 绑定 CardViewHolder2 的数据和视图
             val format = SimpleDateFormat("yyyy年M月d日", Locale.getDefault())
             dataView.text = format.format(card.createDate)
             titleTextView.text = card.title
-
+            tag_style.text = card.eventTag
+            tag_style.text = card.moodTags[0]
             // 使用 Glide 加载并绑定 Uri 到 imageView
             Glide.with(itemView.context)
                 .load(card.coverUri)
--- app/src/main/java/com/bagel/noink/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/bagel/noink/ui/home/HomeViewModel.kt
@@ -10,4 +10,13 @@ class HomeViewModel : ViewModel() {
         value = "记录下今天的故事吧~"
     }
     val text: LiveData<String> = _text
+
+    private val _count_of_card = MutableLiveData<Int>().apply {
+        value = 1
+    }
+    val count_of_card: LiveData<Int> = _count_of_card
+
+    fun updateCount(newValue: Int) {
+        _count_of_card.value = newValue
+    }
 }
